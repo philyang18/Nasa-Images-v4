f8ef776c96a4dc4028d394225336a55d7ce8ee91
Phil Yang <phillip3000@sbcglobal.net>
2019-12-08 13:09:34 -0800

    added two more access keys to API

--- src/NasaAPIs.js
+++ src/NasaAPIs.js
@@ -1,11 +1,21 @@
-// const API_KEY = "RzyT98G4RWi51f3LYNHdbdEzUJkUH7RdAJnQoOd0";
-// const API_KEY = "BfzfbPpRYn7O5rafnhT7BMOC0hUoEV54ybVwWe1a";
-const API_KEY = "PBViMuqFzfpvcXjqnmC6jYR4pqkNbyC0jNUy95Sh";
+const API_KEY1 = "RzyT98G4RWi51f3LYNHdbdEzUJkUH7RdAJnQoOd0";
+const API_KEY2 = "BfzfbPpRYn7O5rafnhT7BMOC0hUoEV54ybVwWe1a";
+const API_KEY3 = "PBViMuqFzfpvcXjqnmC6jYR4pqkNbyC0jNUy95Sh";
 export async function fetchRover(date) {
   try {
-    const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY}`);
+    const response1 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY1}`);
     // const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=MAHLI&api_key=RzyT98G4RWi51f3LYNHdbdEzUJkUH7RdAJnQoOd0`);
-    const json = await response.json();
+    var json = await response.json();
+    if(json.error){
+      const response2 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY2}`);
+    // const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=MAHLI&api_key=RzyT98G4RWi51f3LYNHdbdEzUJkUH7RdAJnQoOd0`);
+      json = await response2.json();
+    }
+    if(json.error){
+      const response3 = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${API_KEY3}`);
+    // const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=MAHLI&api_key=RzyT98G4RWi51f3LYNHdbdEzUJkUH7RdAJnQoOd0`);
+      json = await response3.json();
+    }
     return json.photos;
   } catch {
     return 0;
@@ -14,11 +24,27 @@ export async function fetchRover(date) {
 export async function fetchAPOD(date){
   var response;
   if (!date) {
-    response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&hd=true`);
+    response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY1}&hd=true`);
   } else {
-    response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}&hd=true`);
+    response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY1}&date=${date}&hd=true`);
   }
-  const json = await response.json();
+  var json = await response.json();
+  if(json.error){
+    if (!date) {
+      response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY2}&hd=true`);
+    } else {
+      response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY2}&date=${date}&hd=true`);
+    }
+    json = await response.json();
+  }
+  if(json.error){
+    if (!date) {
+      response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY3}&hd=true`);
+    } else {
+      response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY3}&date=${date}&hd=true`);
+    }
+    json = await response.json();
+  }
 	return json;
 }
 export async function fetchFavorites(){
